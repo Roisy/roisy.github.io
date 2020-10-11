1943c2ba1722e91a8c6d031415e7bc440be41a5e
juanfrancs <juanfran.cabresanch@gmail.com>
2020-10-11 18:32:19 +0200

    Navbar update

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,113 +1,151 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import {FiInstagram} from 'react-icons/fi';
+import {FiYoutube} from 'react-icons/fi';
+import {FiGithub} from 'react-icons/fi';
 
 export class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <div id="contact">
-          <div className="container">
-            <div className="col-md-8">
-              <div className="row">
-                <div className="section-title">
-                  <h2>Get In Touch</h2>
-                  <p>
-                    Please fill out the form below to send us an email and we
-                    will get back to you as soon as possible.
-                  </p>
-                </div>
-                <form action="https://docs.google.com/forms/d/e/1FAIpQLSdMvl0i4vwy0LZVQkv5Aw17eeaxzrIYCU-m_Z0y9RqrXM5PVA/formResponse" target="_self" name="sentMessage" id="contactForm" method="POST">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input id="2005620554" type="text" name="entry.2005620554" className="form-control" placeholder="Name" required/>
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input id="emailAddress" type="email" name="emailAddress" className="form-control" placeholder="Email" required/>
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <textarea id="1288428869" name="entry.1288428869" className="form-control" rows="4" placeholder="Message" required></textarea>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                  <div id="success"></div>
-                  <button type="submit" className="btn btn-custom btn-lg">
-                    Send Message
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div className="col-md-3 col-md-offset-1 contact-info">
-              <div className="contact-item">
-                <h3>Contact Info</h3>
-                <p>
-                  <span>
-                    <i className="fa fa-map-marker"></i> Address
-                  </span>
-                  {this.props.data ? this.props.data.address : "loading"}
-                </p>
-              </div>
-              <div className="contact-item">
-                <p>
-                  <span>
-                    <i className="fa fa-phone"></i> Phone
-                  </span>{" "}
-                  {this.props.data ? this.props.data.phone : "loading"}
-                </p>
-              </div>
-              <div className="contact-item">
-                <p>
-                  <span>
-                    <i className="fa fa-envelope-o"></i> Email
-                  </span>{" "}
-                  {this.props.data ? this.props.data.email : "loading"}
-                </p>
-              </div>
-            </div>
-            <div className="col-md-12">
-              <div className="row">
-                <div className="social">
-                  <ul>
-                    <li>
-                      <a
-                        href={this.props.data ? this.props.data.instagram : "/"}
-                      >
-                        <i className="fa fa-instagram"></i>
-                      </a>
-                    </li>
-                    {/*<li>
-                      <a href={this.props.data ? this.props.data.twitter : "/"}>
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>*/}
-                    <li>
-                      <a href={this.props.data ? this.props.data.youtube : "/"}>
-                        <i className="fa fa-youtube"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div id="footer">
-          <div className="container text-center">
-            <p>
-              &copy; 2020 ROISY. Design by{" "}
-              <a href="http://www.templatewire.com" rel="nofollow">
-                TemplateWire
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div id="contact">
+					<div className="container">
+						<div className="col-md-8">
+							<div className="row">
+								<div className="section-title">
+									<h2>Get In Touch</h2>
+									<p>
+										Please fill out the form below to send us an email and we
+										will get back to you as soon as possible.
+									</p>
+								</div>
+								<form
+									action="https://docs.google.com/forms/d/e/1FAIpQLSdMvl0i4vwy0LZVQkv5Aw17eeaxzrIYCU-m_Z0y9RqrXM5PVA/formResponse"
+									target="_self"
+									name="sentMessage"
+									id="contactForm"
+									method="POST"
+								>
+									<div className="row">
+										<div className="col-md-6">
+											<div className="form-group">
+												<input
+													id="2005620554"
+													type="text"
+													name="entry.2005620554"
+													className="form-control"
+													placeholder="Name"
+													required
+												/>
+												<p className="help-block text-danger"></p>
+											</div>
+										</div>
+										<div className="col-md-6">
+											<div className="form-group">
+												<input
+													id="emailAddress"
+													type="email"
+													name="emailAddress"
+													className="form-control"
+													placeholder="Email"
+													required
+												/>
+												<p className="help-block text-danger"></p>
+											</div>
+										</div>
+									</div>
+									<div className="form-group">
+										<textarea
+											id="1288428869"
+											name="entry.1288428869"
+											className="form-control"
+											rows="4"
+											placeholder="Message"
+											required
+										></textarea>
+										<p className="help-block text-danger"></p>
+									</div>
+									<div id="success"></div>
+									<button type="submit" className="btn btn-custom btn-lg">
+										Send Message
+									</button>
+								</form>
+							</div>
+						</div>
+						<div className="col-md-3 col-md-offset-1 contact-info">
+							<div className="contact-item">
+								<h3>Contact Info</h3>
+								<p>
+									<span>
+										<i className="fa fa-map-marker"></i> Address
+									</span>
+									{this.props.data ? this.props.data.address : 'loading'}
+								</p>
+							</div>
+							<div className="contact-item">
+								<p>
+									<span>
+										<i className="fa fa-phone"></i> Phone
+									</span>{' '}
+									{this.props.data ? this.props.data.phone : 'loading'}
+								</p>
+							</div>
+							<div className="contact-item">
+								<p>
+									<span>
+										<i className="fa fa-envelope-o"></i> Email
+									</span>{' '}
+									{this.props.data ? this.props.data.email : 'loading'}
+								</p>
+							</div>
+						</div>
+						<div className="col-md-12">
+							<div className="row">
+								<div className="social">
+									<ul>
+										<li>
+											<a className="git" href={'https://github.com/roisy/'}>
+												<FiGithub className="fa" size={30} />
+											</a>
+										</li>
+
+										<li>
+											<a
+												class="youtube"
+												href={
+													'https://www.youtube.com/channel/UCnob9ZsHJMc_JS1JkveE5bA/'
+												}
+											>
+												<FiYoutube className="fa" size={30} />
+											</a>
+										</li>
+
+										<li>
+											<a
+												className="insta"
+												href={'https://www.instagram.com/roisy.sa/'}
+											>
+												<FiInstagram size={30} />
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="footer">
+					<div className="container text-center">
+						<p>
+							&copy; 2020 ROISY. Design by{' '}
+							<a href="http://www.templatewire.com" rel="nofollow">
+								TemplateWire
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Contact;
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,74 +1,118 @@
-import React, { Component } from "react";
+import React, {useRef, useLayoutEffect, useState, Component} from 'react';
+import {FiInstagram} from 'react-icons/fi';
+import {FiYoutube} from 'react-icons/fi';
+import {FiGithub} from 'react-icons/fi';
 
 export class Navigation extends Component {
-  render() {
-    return (
-      <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-            >
-              {" "}
-              <span className="sr-only">Toggle navigation</span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-            </button>
-            <a className="navbar-brand page-scroll" id="ROISY-name" href="#page-top">
-              ROISY
-            </a>{" "}
-          </div>
+	constructor(props) {
+		super(props);
+		this.state = {width: 0, height: 0};
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+	}
 
-          <div
-            className="collapse navbar-collapse"
-            id="bs-example-navbar-collapse-1"
-          >
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="#features" className="page-scroll">
-                  Features
-                </a>
-              </li>
-              <li>
-                <a href="#about" className="page-scroll">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#services" className="page-scroll">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#portfolio" className="page-scroll">
-                  Gallery
-                </a>
-              </li>
-              {/*<li>
-                <a href="#testimonials" className="page-scroll">
-                  Testimonials
-                </a>
-              </li>*/}
-              <li>
-                <a href="#team" className="page-scroll">
-                  Team
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="page-scroll">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+	componentDidMount() {
+		this.updateWindowDimensions();
+		window.addEventListener('resize', this.updateWindowDimensions);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateWindowDimensions);
+	}
+
+	updateWindowDimensions() {
+		this.setState({width: window.innerWidth, height: window.innerHeight});
+	}
+	render() {
+		return (
+			<nav id="menu" className="navbar navbar-default navbar-fixed-top">
+				<div className="container">
+					<div className="navbar-header">
+						<button
+							type="button"
+							className="navbar-toggle collapsed"
+							data-toggle="collapse"
+							data-target="#bs-example-navbar-collapse-1"
+						>
+							{' '}
+							<span className="sr-only">Toggle navigation</span>{' '}
+							<span className="icon-bar"></span> <span className="icon-bar"></span>{' '}
+							<span className="icon-bar"></span>{' '}
+						</button>
+						<a className="navbar-brand page-scroll" href="#page-top">
+							ROISY
+						</a>{' '}
+					</div>
+
+					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						{this.state.width > this.state.height ? (
+							<ul className="nav navbar-nav navbar-right" style={{marginLeft: '7%'}}>
+								<li>
+									<a className="git" href={'https://github.com/roisy/'}>
+										<FiGithub className="fa" size={24} />
+									</a>
+								</li>
+
+								<li>
+									<a
+										class="youtube"
+										href={
+											'https://www.youtube.com/channel/UCnob9ZsHJMc_JS1JkveE5bA/'
+										}
+									>
+										<FiYoutube className="fa" size={24} />
+									</a>
+								</li>
+
+								<li>
+									<a
+										className="insta"
+										href={'https://www.instagram.com/roisy.sa/'}
+									>
+										<FiInstagram size={24} />
+									</a>
+								</li>
+							</ul>
+						) : (
+							<div></div>
+						)}
+						<ul className="nav navbar-nav navbar-right">
+							<li>
+								<a href="#features" className="page-scroll">
+									Features
+								</a>
+							</li>
+							<li>
+								<a href="#about" className="page-scroll">
+									About
+								</a>
+							</li>
+							<li>
+								<a href="#services" className="page-scroll">
+									Services
+								</a>
+							</li>
+							<li>
+								<a href="#portfolio" className="page-scroll">
+									Gallery
+								</a>
+							</li>
+
+							<li>
+								<a href="#team" className="page-scroll">
+									Team
+								</a>
+							</li>
+							<li>
+								<a href="#contact" className="page-scroll">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 }
 
 export default Navigation;
