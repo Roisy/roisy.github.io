6bbe06e9f95873671e2ab5c321993cbca1f72189
Fernando M. Quintana Velázquez <ferquinve@gmail.com>
2020-10-29 13:31:16 +0100

    Update photos and text

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,7 @@ export class about extends Component {
         <div id="about">
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-md-6"> <img src="img/about.jpg" className="img-responsive" alt=""/> </div>
+            <div className="col-xs-12 col-md-6"> <img src="img/roisy_team.jpg" className="img-responsive" alt=""/> </div>
             <div className="col-xs-12 col-md-6">
               <div className="about-text">
                 <h2>Sobre nosotros</h2>
